3e7a5084b7bc94d30af64ffbcab9491ff19c5eac
Artyom Sadyrin <artsiomsadyryn@gmail.com>
2019-10-24 12:52:46 +0300

    Added a table view to Jogs Screen and with temporary data

--- Jog Tracker/Controller/JogsViewController.swift
+++ Jog Tracker/Controller/JogsViewController.swift
@@ -8,16 +8,64 @@
 
 import UIKit
 
-class JogsViewController: UIViewController
+class JogsViewController: UIViewController, UITableViewDataSource, UITableViewDelegate
 {
+    
+    // MARK: Properties
+    
+    private struct Jog {
+        let identifier: String = UUID().uuidString
+        var distance: Double
+        var time: Int
+        var date: String
+        
+    }
+    
+    @IBOutlet weak var jogsTableView: UITableView!
+    
+    private var jogs: [Jog] = [
+        Jog(distance: 2.2, time: 10, date: "July 10"),
+        Jog(distance: 3.9, time: 20, date: "August 1"),
+        Jog(distance: 4.4, time: 32, date: "September 9"),
+        Jog(distance: 1.3, time: 2, date: "September 30"),
+        Jog(distance: 10.2, time: 90, date: "October 31")
+    ]
+    
+    // MARK: General Methods
 
     override func viewDidLoad() {
         super.viewDidLoad()
         self.modalPresentationStyle = .fullScreen
+        jogsTableView.delegate = self
+        jogsTableView.dataSource = self
     }
+    
+    // MARK: Action Methods
     
     @IBAction func backBarButtonItemPressed(_ sender: UIBarButtonItem) {
         presentingViewController?.dismiss(animated: true, completion: nil)
+    }
+    
+    // MARK: Table View Data Source
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return jogs.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cellIdentifier = "JogTableViewCell"
+        
+        
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: cellIdentifier, for: indexPath) as? JogTableViewCell else {
+            fatalError("The dequeued cell is not an instance of JogTableViewCell.")
+        }
+        let jog = jogs[indexPath.row]
+        cell.identifierLabel.text = "Jog #\(jog.identifier)"
+        cell.distanceLabel.text = "Distance: \(jog.distance)"
+        cell.timeLabel.text = "Time: \(jog.time)"
+        cell.dateLabel.text = "Date: \(jog.date)"
+        
+        return cell
     }
     
     
---
+++ Jog Tracker/View/JogTableViewCell.swift
@@ -0,0 +1,31 @@
+//
+//  JogTableViewCell.swift
+//  Jog Tracker
+//
+//  Created by Artyom Sadyrin on 10/24/19.
+//  Copyright © 2019 Artyom Sadyrin. All rights reserved.
+//
+
+import UIKit
+
+class JogTableViewCell: UITableViewCell {
+    
+    @IBOutlet weak var identifierLabel: UILabel!
+    @IBOutlet weak var distanceLabel: UILabel!
+    
+    @IBOutlet weak var timeLabel: UILabel!
+    
+    @IBOutlet weak var dateLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
