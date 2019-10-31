ef9d8a0c8cf6e64d44d12615948bb08b1af51f4d
Artyom Sadyrin <artsiomsadyryn@gmail.com>
2019-10-31 12:31:59 +0300

    Moved general extensions to a separate file

---
+++ Jog Tracker/Controller/Extensions.swift
@@ -0,0 +1,110 @@
+//
+//  Extensions.swift
+//  Jog Tracker
+//
+//  Created by Artyom Sadyrin on 10/31/19.
+//  Copyright © 2019 Artyom Sadyrin. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+extension UIViewController {
+    var contents: UIViewController {
+        if let navcon = self as? UINavigationController {
+            return navcon.visibleViewController ?? self
+        } else {
+            return self
+        }
+    }
+}
+
+extension Date {
+    func performDateFormattingToString() -> String? {
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "MMM d, yyyy"
+        let dateInString = dateFormatter.string(from: self)
+        return dateInString
+    }
+}
+
+extension DateInterval {
+    func formatToString() -> String? {
+        let formatter = DateIntervalFormatter()
+        formatter.dateTemplate = "yyyy-MM-dd"
+        let dateIntervalInString = formatter.string(from: self)
+        return dateIntervalInString
+    }
+}
+
+extension UIViewController {
+    func hideKeyboardOnTouchUpInside() {
+        let tap: UITapGestureRecognizer = UITapGestureRecognizer( target: self, action: #selector(UIViewController.dismissKeyboard))
+        tap.cancelsTouchesInView = false
+        view.addGestureRecognizer(tap)
+    }
+    @objc func dismissKeyboard() {
+        view.endEditing(true)
+    }
+}
+
+extension UIButton {
+    @IBInspectable var cornerRadius: CGFloat {
+        get {
+            return layer.cornerRadius
+        }
+        set {
+            layer.cornerRadius = newValue
+            layer.masksToBounds = newValue > 0
+        }
+    }
+    
+    @IBInspectable var borderWidth: CGFloat {
+        get {
+            return layer.borderWidth
+        }
+        set {
+            layer.borderWidth = newValue
+        }
+    }
+    
+    @IBInspectable var borderColor: UIColor? {
+        get {
+            return UIColor(cgColor: layer.borderColor!)
+        }
+        set {
+            layer.borderColor = newValue?.cgColor
+        }
+    }
+}
+
+extension UITextField {
+    var isEmpty: Bool {
+        if let text = self.text, text.isEmpty {
+            return true
+        } else {
+            return false
+        }
+    }
+}
+
+extension Array where Element: Dateable {
+    func groupedBy(dateComponent: Calendar.Component) -> [DateInterval: [Element]] {
+        let initial: [DateInterval: [Element]] = [:]
+        let groupedByDateComponents = reduce(into: initial) { accumulator, current in
+            guard let currentDate = current.date else {
+                return
+            }
+            
+            let calendar = Calendar.autoupdatingCurrent
+            
+            guard let dateInterval = calendar.dateInterval(of: dateComponent, for: currentDate) else {
+                return
+            }
+            let existing = accumulator[dateInterval] ?? []
+            accumulator[dateInterval] = existing + [current]
+        }
+        
+        return groupedByDateComponents
+    }
+}
--- Jog Tracker/Controller/LoginViewController.swift
+++ Jog Tracker/Controller/LoginViewController.swift
@@ -134,75 +134,6 @@ class LoginViewController: UIViewController, UITextFieldDelegate {
 
 }
 
-extension UIViewController {
-    var contents: UIViewController {
-        if let navcon = self as? UINavigationController {
-            return navcon.visibleViewController ?? self
-        } else {
-            return self
-        }
-    }
-}
-
-extension Date {
-    func performDateFormattingToString() -> String? {
-        let dateFormatter = DateFormatter()
-        dateFormatter.dateFormat = "MMM d, yyyy"
-        let dateInString = dateFormatter.string(from: self)
-        return dateInString
-    }
-}
-
-extension DateInterval {
-    func formatToString() -> String? {
-        let formatter = DateIntervalFormatter()
-        formatter.dateTemplate = "yyyy-MM-dd"
-        let dateIntervalInString = formatter.string(from: self)
-        return dateIntervalInString
-    }
-}
-
-extension UIViewController {
-    func hideKeyboardOnTouchUpInside() {
-        let tap: UITapGestureRecognizer = UITapGestureRecognizer( target: self, action: #selector(UIViewController.dismissKeyboard))
-        tap.cancelsTouchesInView = false
-        view.addGestureRecognizer(tap)
-    }
-    @objc func dismissKeyboard() {
-        view.endEditing(true)
-    }
-}
-
-extension UIButton {
-    @IBInspectable var cornerRadius: CGFloat {
-        get {
-            return layer.cornerRadius
-        }
-        set {
-            layer.cornerRadius = newValue
-            layer.masksToBounds = newValue > 0
-        }
-    }
-    
-    @IBInspectable var borderWidth: CGFloat {
-        get {
-            return layer.borderWidth
-        }
-        set {
-            layer.borderWidth = newValue
-        }
-    }
-    
-    @IBInspectable var borderColor: UIColor? {
-        get {
-            return UIColor(cgColor: layer.borderColor!)
-        }
-        set {
-            layer.borderColor = newValue?.cgColor
-        }
-    }
-}
-
 extension LoginViewController.LoginError: LocalizedError {
     var errorDescription: String? {
         switch self {
@@ -211,38 +142,5 @@ extension LoginViewController.LoginError: LocalizedError {
         case .unknownSegue:
             return NSLocalizedString("Unexpected Segue Identifier.\nPlease report to the developer", comment: "Login Failed")
         }
-    }
-}
-
-extension UITextField {
-    var isEmpty: Bool {
-        if let text = self.text, text.isEmpty {
-            return true
-        } else {
-            return false
-        }
-    }
-}
-
-extension Array where Element: Dateable {
-    func groupedBy(dateComponent: Calendar.Component) -> [DateInterval: [Element]] {
-        let initial: [DateInterval: [Element]] = [:]
-        let groupedByDateComponents = reduce(into: initial) { accumulator, current in
-            guard let currentDate = current.date else {
-                os_log(.error, log: OSLog.default, "Couldn't get date")
-                return
-            }
-            
-            let calendar = Calendar.autoupdatingCurrent
-            
-            guard let dateInterval = calendar.dateInterval(of: dateComponent, for: currentDate) else {
-                os_log(.error, log: OSLog.default, "Couldn't get date from date compoments")
-                return
-            }
-            let existing = accumulator[dateInterval] ?? []
-            accumulator[dateInterval] = existing + [current]
-        }
-        
-        return groupedByDateComponents
     }
 }
